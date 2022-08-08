93c519f1dffc5b0762b6617c00a6b610666ff363
Felipe Alves <profelipealves@outlook.com>
2022-08-08 13:21:49 -0300

    adiciona evento de clique no botão

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ let numero2 = document.getElementById('numero2');
 let botao = document.getElementById('calcular');
 let res = document.getElementById('resultado');
 
+function calcular(n1, n2) {
+    if(typeof n1 === 'number' && typeof n2 === 'number') {
+    return n1 + n2;
+    } else {
+        n1 = parseInt(n1);
+        n2 = parseInt(n2);
+        return n1 + n2;
+}
+}
+
+botao.addEventListener('click', function() {
+    res.innerHTML = calcular(numero1.value, numero2.value);
+});
