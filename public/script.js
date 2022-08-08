a96d380a48bafe8b30202e3fbadf6f6eb9c8b1a7
Felipe Alves <profelipealves@outlook.com>
2022-08-08 15:38:17 -0300

    update

--- public/script.js
+++ public/script.js
@@ -3,13 +3,8 @@ var numero2 = document.getElementById("numero2");
 var botao = document.getElementById("calcular");
 var res = document.getElementById("resultado");
 function calcular(n1, n2) {
-    if (typeof n1 === "number" && typeof n2 === "number") {
-        return n1 + n2;
-    }
-    else {
-        return +n1 + +n2;
-    }
+    return +n1 + +n2;
 }
 botao.addEventListener("click", function () {
-    res.innerHTML = calcular(numero1.value, numero2.value);
+    res.innerHTML = calcular(+numero1.value, +numero2.value).toString();
 });
--- src/script.ts
+++ src/script.ts
@@ -3,14 +3,10 @@ let numero2 = document.getElementById("numero2") as HTMLInputElement;
 let botao = document.getElementById("calcular");
 let res = document.getElementById("resultado");
 
-function calcular(n1, n2) {
-  if (typeof n1 === "number" && typeof n2 === "number") {
-    return n1 + n2;
-  } else {
-    return +n1 + +n2;
-  }
+function calcular(n1: number, n2: number) {
+  return +n1 + +n2;
 }
 
-botao.addEventListener("click", function() {
-  res.innerHTML = calcular(numero1.value, numero2.value);
-}   );
+botao.addEventListener("click", function () {
+  res.innerHTML = calcular(+numero1.value, +numero2.value).toString();
+});
