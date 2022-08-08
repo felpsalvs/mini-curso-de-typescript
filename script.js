c4d9ea223f83faae30537ee6770720368101d175
Felipe Alves <profelipealves@outlook.com>
2022-08-08 13:20:10 -0300

    adiciona variaveis

---
+++ script.js
@@ -0,0 +1,5 @@
+let numero1 = document.getElementById('numero1');
+let numero2 = document.getElementById('numero2');
+let botao = document.getElementById('calcular');
+let res = document.getElementById('resultado');
+
