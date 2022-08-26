b171a1b835de71d8839bbfa3926d07d5f87b6fcf
Felipe Alves <profelipealves@outlook.com>
2022-08-26 10:38:48 -0300

    adiciona tipos básicos

--- src/type.ts
+++ src/type.ts
@@ -1,4 +1,4 @@
-//boolean (true or false)
+//boolean (true or false) ex: função que retorna true ou false
 let isDone: boolean
 isDone = false
 
@@ -6,7 +6,7 @@ isDone = false
 let nam: string
 nam = 'foo'
 
-// number (int or float, hex, binary)
+// number (int or float, hex, binary) ex: 0xf00d, 0b1010, 0o744
 let num: number
 num = 1
 
@@ -33,3 +33,22 @@ enum Color {
 // any (qualquer coisa) NÃO É RECOMENDADO
 let notSure: any
 notSure = 1
+
+// void (vazio) ex: função que não retorna nada
+function warnUser(): void {
+    console.log('This is my warning message')
+}
+
+// null e undefined ex (null e undefined são subtipos de todos os outros tipos)
+type Bla = string | null | undefined
+
+// never (nunca) ex: função que lança exceção ou nunca retorna
+function error(message: string): never {
+    throw new Error(message)
+}
+
+// object (qualquer coisa que não seja number, string, boolean, bigint, symbol, null ou undefined)
+let cart: object;
+cart = {
+    key: 'fi'
+}
