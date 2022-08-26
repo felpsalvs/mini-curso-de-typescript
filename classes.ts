38c0ad426d301805804d7f7a940d1d89d4f2ed1d
Felipe Alves <profelipealves@outlook.com>
2022-08-26 13:05:50 -0300

    adicionando classes CharAccount

--- classes.ts
+++ classes.ts
@@ -1,17 +1,45 @@
-class UserAccount {
-  name: string;
-  age: number;
+abstract class UserAccount {
+  public name: string;
+  protected age: number;
   constructor(name: string, age: number) {
-      this.name = name;
-      this.age = age;
-    }
-    logDetails(): void {
-        console.log(`The player ${this.name} has the id ${this.id}`);
-        }
+    this.name = name;
+    this.age = age;
+  }
+  logDetails(): void {
+    console.log(`The player ${this.name} has the id ${this.age}`);
+  }
 }
 
-const john = new UserAccount("John", 123);
-console.log(john);
-console.log(john.age);
-john.logDetails();
+class CharAccount extends UserAccount {
+  private nickname: string;
+  level: number;
+  constructor(name: string, age: number, nickname: string, level: number) {
+    super(name, age);
+    this.nickname = nickname;
+    this.level = level;
+  }
+  get getLevel() {
+    console.log("------GET------");
+    return this.level;
+  }
+  set setLevel(level: number) {
+    this.level = level;
+  }
+}
 
+// logCharDetails = (char: CharAccount): void => {
+//     console.log(`The player ${char.name} has the id ${char.level}`);
+//     }
+
+// const john = new UserAccount("John", 123);
+// console.log(john);
+// console.log(john.age);
+// john.logDetails();
+
+const john = new CharAccount("John", 123, "johnmaster", 80);
+john.logDetails();
+console.log(john.level);
+// john.logCharDetails();
+console.log(john.getLevel);
+john.setLevel = 499;
+console.log(john);
